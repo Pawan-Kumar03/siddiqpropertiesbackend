975af5d51be86500abb59aa1df974671f26f351b
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-08-12 15:22:07 +0500

    image not required for test purpose

--- models/Listing.js
+++ models/Listing.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const ListingSchema = new mongoose.Schema({
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   title: { type: String, required: true },
   price: { type: String, required: true },
   city: { type: String, required: true },
