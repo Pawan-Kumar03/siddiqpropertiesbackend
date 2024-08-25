2e7805f01af19743515bfb31765c416d1b286534
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-08-25 13:39:25 +0500

    route to get user properties

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,20 @@
-// middleware/auth.js
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js'; // Adjust the path if needed
+import User from '../models/User'; // Adjust the path if needed
 
 const auth = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
+  const token = req.header('Authorization');
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Remove 'Bearer ' from token
+    const bearerToken = token.replace('Bearer ', '');
+    const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.userId).select('-password');
     next();
   } catch (error) {
+    console.error('Error in auth middleware:', error);
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
