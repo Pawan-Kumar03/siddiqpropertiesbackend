68cb93f21cb7cc37f3621db596eee5da6050353b
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-20 23:28:37 +0500

    increase payload size

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ import { body, validationResult } from 'express-validator';
 dotenv.config();
 
 const app = express();
+// Middleware to handle large payloads
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 // Authentication Middleware
 const auth = async (req, res, next) => {
   console.log('request: ',req)
